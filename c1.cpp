072997880ab9d2623cbdb35cf4f5e2ff4a576758
boooori <138274199+boooori@users.noreply.github.com>
2024-03-07 20:54:08 +0900

    Add files via upload

---
+++ c1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+	//printf("Hello World");
+	/*
+	printf("치동초등학교\n");
+	printf("6학년 6반\n");
+	printf("현보민");
+	*/
+	/*
+	printf("Hello C3coding\n");
+	printf("Hello\n C3coding\n");
+	printf("Hello\b C3coding\n");
+	printf("Hello\r C3coding\n");
+	printf("Hello\t C3coding\n");
+	*/
+	/*
+	printf("\'");
+	printf("\"");
+	printf("\\");
+	printf("%%");
+	*/
+	/*
+	printf("12345678\n");
+	printf("ABCDEFG\n");
+	printf("대한민국\n");
+	printf("abcdefg\n");
+	printf("◎◆♣♥\n");
+	printf("!@#$%^&*\n");
+	printf("12345678\n");
+	*/
+	printf("%%Solar Tracking System%%\n");
+	printf("\\Smart Devices\\\n");
+	printf("\"Matrix\"\n");
+	printf("\'Hello Robot\'\n");
+	return 0;	
+}
