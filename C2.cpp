7a4d40cd166504c0ac85c0364cabeca2be106f81
boooori <138274199+boooori@users.noreply.github.com>
2024-03-14 20:46:15 +0900

    Add files via upload

---
+++ C2.cpp
@@ -0,0 +1,9 @@
+#include  <stdio.h>
+
+int main(){
+	printf("나의 이름은 %s 입니다.\n","현보민");
+	printf("나는 %d살 입니다.\n",13);
+	printf("나의 혈액형은 %c입니다.\n",'B');
+	printf("나의 시력은 %.1lf/%.1lf입니다.\n",0.1,0.3);
+	return 0;
+}
