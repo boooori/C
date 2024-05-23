4f2fee3a73bb33340617633f85c7d60fae705d5b
boooori <138274199+boooori@users.noreply.github.com>
2024-05-23 20:53:28 +0900

    Add files via upload

---
+++ C11.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+	/*
+	for(int i = 6; i>=1;i--) 
+	{
+		printf("%d«–≥‚",i);
+		for(int j=1; j<=7; j++)
+		{
+			printf("%dπ›",j);
+		}
+		printf("\n");
+	}
+	*/
+	/*
+	for(int i=1; i<=5; i++)
+	{
+		for(int j = 1; j<=i; j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	*/
+	/*
+	for(int i=5; i>=1; i--)
+	{
+		for(int j = 1; j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	*/
+	for(int i=1; i>=5; i++)
+	{
+		for(int j = 4; j<=i;j--)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
